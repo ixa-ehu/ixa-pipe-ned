54e4c22ee68392f142f85736b047e0d234ce7bb1
Rodrigo Agerri <agerri.rodrigo@gmail.com>
2013-06-19 10:19:29 +0200

    host option added

--- src/main/java/ixa/pipe/ned/CLI.java
+++ src/main/java/ixa/pipe/ned/CLI.java
@@ -20,8 +20,8 @@ public class CLI {
 
         // create Argument Parser
         ArgumentParser parser = ArgumentParsers.newArgumentParser(
-            "ehu-ned-1.0.jar").description(
-            "ehu-ned-1.0 is a multilingual Named Entity Disambiguation module "
+            "ixa-pipe-ned-1.0.jar").description(
+            "ixa-pipe-ned-1.0 is a multilingual Named Entity Disambiguation module "
                 + "developed by IXA NLP Group based on DBpedia Spotlight API.\n");
 
         // specify port
@@ -30,9 +30,12 @@ public class CLI {
             .choices("2010","2020","2030","2040","2050","2060")
             .required(true)
             .help(
-                "It is REQUIRED to choose a port number. Port numbers are assigned" +
-                "alphabetically by language code: de:2010,en:2020,es:2030,fr:2040,it:2050,nl:2060");
+                "It is REQUIRED to choose a port number. Port numbers are assigned " +
+                "alphabetically by language code: de:2010, en:2020, es:2030, fr:2040, it:2050, nl:2060");
 
+        parser.addArgument("-H", "--host").setDefault("http://localhost").help("Choose hostname in which dbpedia-spotlight rest " +
+        		"server is being executed; this value defaults to 'http://localhost'");
+        
         /*
          * Parse the command line arguments
          */
@@ -43,15 +46,16 @@ public class CLI {
         } catch (ArgumentParserException e) {
           parser.handleError(e);
           System.out
-              .println("Run java -jar target/ehu-ned-1.0.jar -help for details");
+              .println("Run java -jar target/ixa-pipe-ned-1.0.jar -help for details");
           System.exit(1);
         }
 
         /*
-         * Load language and headFinder parameters
+         * Load port and host parameters; host defaults to http://localhost
          */
 
-        String port = parsedArguments.getString("port"); 	
+        String port = parsedArguments.getString("port");
+        String host = parsedArguments.getString("host");
 
     	Annotate annotator = new Annotate();
 		// Input
@@ -62,7 +66,7 @@ public class CLI {
 		stdInReader = new BufferedReader(new InputStreamReader(System.in,"UTF-8"));
 		w = new BufferedWriter(new OutputStreamWriter(System.out,"UTF-8"));
 		KAFDocument kaf = KAFDocument.createFromStream(stdInReader);
-		annotator.disambiguateNEsToKAF(kaf,port);
+		annotator.disambiguateNEsToKAF(kaf, host, port);
 		w.write(kaf.toString());
 		w.close();
 	    } 
